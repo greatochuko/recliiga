7a6040312815281f38339b067ce1dae28f68846b
Great Ogheneochuko <greatochuko123@gmail.com>
2025-11-03 10:10:09 +0100

    fix: refactor NotificationsPopover component for improved readability and organization; correct variable names and enhance layout

--- src/components/sidebar/NotificationsPopover.tsx
+++ src/components/sidebar/NotificationsPopover.tsx
@@ -14,7 +14,6 @@ import { getInitials } from "@/lib/utils";
 export function NotificationsPopover() {
   const [isOpen, setIsOpen] = useState(false);
   const [loading, setLoading] = useState(false);
-
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   const { data, isLoading, refetch } = useQuery({
@@ -23,12 +22,10 @@ export function NotificationsPopover() {
   });
 
   const notifications = data?.data || [];
+
   const joinRequests = notifications.filter((n) => n.type === "LEAGUE_REQUEST");
-  const joinLeagutNotifications = notifications.filter(
+  const joinLeagueNotifications = notifications.filter(
     (n) => n.type === "JOIN_LEAGUE",
-  );
-  const otherNotifications = notifications.filter(
-    (n) => n.type !== "LEAGUE_REQUEST",
   );
   const draftNotifications = notifications.filter(
     (n) => n.type === "PLAYER_DRAFTED",
@@ -48,11 +45,13 @@ export function NotificationsPopover() {
   const inputResultNotifications = notifications.filter(
     (n) => n.type === "INPUT_RESULTS",
   );
+  const otherNotifications = notifications.filter(
+    (n) => n.type !== "LEAGUE_REQUEST",
+  );
 
-  const unreadNotifications = notifications.filter((notif) => !notif.isRead);
-
+  const unreadNotifications = notifications.filter((n) => !n.isRead);
   const unreadNotificationCount = notifications.filter(
-    (notif) => !notif.isRead || notif.type === "LEAGUE_REQUEST",
+    (n) => !n.isRead || n.type === "LEAGUE_REQUEST",
   ).length;
 
   async function toggleDropdown() {
@@ -61,20 +60,20 @@ export function NotificationsPopover() {
 
   async function markAllAsRead() {
     setLoading(true);
-    await readAllNotifications(unreadNotifications.map((notif) => notif.id));
+    await readAllNotifications(unreadNotifications.map((n) => n.id));
     refetch();
     setLoading(false);
   }
 
   useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
+    function handleClickOutside(e: MouseEvent) {
       if (
         dropdownRef.current &&
-        !dropdownRef.current.contains(event.target as Node)
+        !dropdownRef.current.contains(e.target as Node)
       ) {
         setIsOpen(false);
       }
-    };
+    }
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
@@ -89,7 +88,7 @@ export function NotificationsPopover() {
 
   return (
     <div className="relative" ref={dropdownRef}>
-      <button className="flex items-center gap-2 p-2" onClick={toggleDropdown}>
+      <button onClick={toggleDropdown} className="flex items-center gap-2 p-2">
         <Bell className="h-5 w-5 text-gray-500" />
         {unreadNotificationCount > 0 && (
           <span className="ml-1 rounded-full bg-orange-500 px-2 py-0.5 text-xs text-white">
@@ -99,9 +98,9 @@ export function NotificationsPopover() {
       </button>
 
       {isOpen && (
-        <div className="absolute left-0 z-10 mt-2 overflow-y-auto rounded-md border border-gray-200 bg-white shadow-lg">
+        <div className="absolute left-0 z-10 mt-2 w-[340px] overflow-hidden rounded-md border border-gray-200 bg-white shadow-lg">
           {notifications.length > 0 ? (
-            <div className="max-h-[340px] w-[340px]">
+            <div className="flex max-h-[400px] flex-col overflow-y-auto">
               {joinRequests.length > 0 && (
                 <>
                   <div className="border-b border-gray-100 p-4 font-medium">
@@ -116,165 +115,144 @@ export function NotificationsPopover() {
                   ))}
                 </>
               )}
+
               {otherNotifications.length > 0 && (
                 <>
-                  <div className="sticky top-0 border-b border-gray-100 bg-white p-4 font-medium">
+                  <div className="sticky top-0 z-10 border-b border-gray-100 bg-white p-4 font-medium">
                     Updates
                   </div>
-                  <div className="max-h-[400px] overflow-y-auto">
-                    {joinLeagutNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <div className="mr-2 h-8 w-8 overflow-hidden rounded-full bg-gray-200">
-                          {notif.initiator.avatar_url ? (
-                            <img
-                              src={notif.initiator.avatar_url}
-                              alt={notif.initiator.full_name}
-                              className="h-full w-full object-cover"
-                            />
-                          ) : (
-                            <div className="flex h-full w-full items-center justify-center text-xs font-medium text-gray-700">
-                              {getInitials(notif.initiator.full_name)}
+
+                  {[
+                    ...joinLeagueNotifications,
+                    ...draftNotifications,
+                    ...resultNotifications,
+                    ...ratingNotifications,
+                    ...selectCaptainNotifications,
+                    ...captainSelectedNotifications,
+                    ...inputResultNotifications,
+                  ].map((notif) => {
+                    const commonClasses =
+                      "flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50";
+                    switch (notif.type) {
+                      case "JOIN_LEAGUE":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <div className="mr-2 h-8 w-8 overflow-hidden rounded-full bg-gray-200">
+                              {notif.initiator.avatar_url ? (
+                                <img
+                                  src={notif.initiator.avatar_url}
+                                  alt={notif.initiator.full_name}
+                                  className="h-full w-full object-cover"
+                                />
+                              ) : (
+                                <div className="flex h-full w-full items-center justify-center text-xs font-medium text-gray-700">
+                                  {getInitials(notif.initiator.full_name)}
+                                </div>
+                              )}
                             </div>
-                          )}
-                        </div>
-                        <div className="flex-1">
-                          <p className="text-sm font-medium">
-                            {notif.initiator.full_name}{" "}
-                            <span className="font-normal">
-                              {" "}
+                            <div className="flex-1 text-sm">
+                              <span className="font-medium">
+                                {notif.initiator.full_name}
+                              </span>{" "}
                               Joined {notif.league.name}
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {draftNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <UserPlusIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            You have been drafted to{" "}
-                            <span className="font-medium">
-                              {notif.team.name}
-                            </span>{" "}
-                            for{" "}
-                            <span className="font-medium">
-                              {notif.event.title}
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {resultNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <TrophyIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            Results are out for{" "}
-                            <span className="font-medium">
-                              {notif.event.title}.
-                            </span>{" "}
-                            Check out the latest scores and rankings
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {ratingNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <StarIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            It's time to rate your teammates from{" "}
-                            <span className="font-medium">
-                              {notif.event.title}.
-                            </span>{" "}
-                            - share your feedback now!
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {selectCaptainNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <SparklesIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            Game on! Select the Captain for{" "}
-                            <span className="font-medium">
-                              {notif.event.title}.
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {captainSelectedNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <SparklesIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            You have been selected as the captain for{" "}
-                            <span className="font-medium">
-                              {notif.team.name}
-                            </span>
-                            {" in "}
-                            <span className="font-medium">
-                              {notif.event.title}.
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                    {inputResultNotifications.map((notif) => (
-                      <div
-                        key={notif.id}
-                        className="flex cursor-default items-center border-b border-gray-100 px-4 py-3 duration-200 hover:bg-gray-50"
-                      >
-                        <SparklesIcon className="mr-2 mt-1 h-4 w-4 self-start text-accent-orange" />
-                        <div className="flex-1">
-                          <p className="text-sm">
-                            It ain&apos;t over till the fat lady sings. Did she?
-                            Input your results for{" "}
-                            <span className="font-medium">
-                              {notif.event.title}.
-                            </span>{" "}
-                            if it has ended.
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
+                            </div>
+                          </div>
+                        );
+                      case "PLAYER_DRAFTED":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <UserPlusIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              You have been drafted to{" "}
+                              <span className="font-medium">
+                                {notif.team.name}
+                              </span>{" "}
+                              for{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>
+                            </div>
+                          </div>
+                        );
+                      case "RESULT_READY":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <TrophyIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              Results are out for{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>
+                              . Check out the latest scores and rankings
+                            </div>
+                          </div>
+                        );
+                      case "RATE_TEAMMATES":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <StarIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              It’s time to rate your teammates from{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>{" "}
+                              – share your feedback now!
+                            </div>
+                          </div>
+                        );
+                      case "SELECT_CAPTAIN":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <SparklesIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              Game on! Select the Captain for{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>
+                              .
+                            </div>
+                          </div>
+                        );
+                      case "CAPTAIN_SELECTED":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <SparklesIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              You have been selected as captain for{" "}
+                              <span className="font-medium">
+                                {notif.team.name}
+                              </span>{" "}
+                              in{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>
+                              .
+                            </div>
+                          </div>
+                        );
+                      case "INPUT_RESULTS":
+                        return (
+                          <div key={notif.id} className={commonClasses}>
+                            <SparklesIcon className="mr-2 mt-1 h-4 w-4 text-accent-orange" />
+                            <div className="flex-1 text-sm">
+                              It ain’t over till the fat lady sings. Input your
+                              results for{" "}
+                              <span className="font-medium">
+                                {notif.event.title}
+                              </span>{" "}
+                              if it has ended.
+                            </div>
+                          </div>
+                        );
+                      default:
+                        return null;
+                    }
+                  })}
                 </>
               )}
-
-              <div className="sticky bottom-0 border-t border-gray-100 bg-white p-2">
-                <button
-                  onClick={markAllAsRead}
-                  disabled={!unreadNotifications.length || loading}
-                  className="w-full rounded-md bg-accent-orange px-4 py-2 text-sm font-medium text-white duration-200 hover:bg-accent-orange/90 disabled:pointer-events-none disabled:bg-accent-orange/50"
-                >
-                  Mark All as Read
-                </button>
-              </div>
             </div>
           ) : (
-            <div className="flex h-[280px] w-[280px] flex-col items-center justify-center gap-2 p-4 text-center">
+            <div className="flex h-[280px] flex-col items-center justify-center gap-2 p-4 text-center">
               <Bell className="h-10 w-10 text-accent-orange" />
               <p className="mt-2 font-medium text-gray-500">
                 You're all caught up!
@@ -282,6 +260,18 @@ export function NotificationsPopover() {
               <p className="text-sm text-gray-400">
                 No new notifications at the moment.
               </p>
+            </div>
+          )}
+
+          {notifications.length > 0 && (
+            <div className="sticky bottom-0 border-t border-gray-100 bg-white p-2">
+              <button
+                onClick={markAllAsRead}
+                disabled={!unreadNotifications.length || loading}
+                className="w-full rounded-md bg-accent-orange px-4 py-2 text-sm font-medium text-white duration-200 hover:bg-accent-orange/90 disabled:pointer-events-none disabled:bg-accent-orange/50"
+              >
+                Mark All as Read
+              </button>
             </div>
           )}
         </div>
